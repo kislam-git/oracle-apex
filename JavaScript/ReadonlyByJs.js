0a00424f372874f0b0f87f656b0aba12de70447c
kislam-git <kislam.pro@gmail.com>
2025-02-25 11:36:43 +0600

    Create ReadonlyByJs.js

---
+++ JavaScript/ReadonlyByJs.js
@@ -0,0 +1,5 @@
+$x('P505_REPORT_CASH_BAL').readOnly="readOnly";
+
+/*
+https://community.oracle.com/tech/developers/discussion/4183629/how-to-set-a-select-list-to-read-only-not-disabled-using-javascript
+*/
