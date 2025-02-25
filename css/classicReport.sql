bbe796dd1037d201ad46c4af27e42239af4d8dcb
kislam-git <kislam.pro@gmail.com>
2025-02-25 11:32:06 +0600

    Create classicReport.sql

---
+++ css/classicReport.sql
@@ -0,0 +1,7 @@
+------------ COLUMN WIDTH
+<div style="display:block; width:110px">#CLS_LOAN_PRN#</div>
+
+Link Attributes 
+------------------
+class="t-Button t-Button--hot t-Button--simple t-Button--stretch" title="লেজার চেক করুন"
+class="t-Button t-Button--success t-Button--stretch" title="বারের সকল সমিতি দেখুন"
